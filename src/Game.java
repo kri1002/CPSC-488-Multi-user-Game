eabb4ebc0691b21146b0136a103b502fffafcc67
jdc1019 <70229639+jdc1019@users.noreply.github.com>
2021-03-02 16:29:38 -0500

    Made all tokens movable

--- src/Game.java
+++ src/Game.java
@@ -22,13 +22,14 @@ public class Game extends JPanel{
         		 if(lastButtonPressed!=null){//if two tiles were clicked
         			 if(lastButtonPressed !=currentButton){ //if different tiles clicked
         				 if(lastButtonPressed.getIcon()!=null&&currentButton.getIcon()==null){//if first tile had icon and second was empty
-        					 try{
-        						Image img = ImageIO.read(Game.class.getResource("/images/BlueCircle.png")); //will have to change image src to variable so it can work with any token
+        					 //try{
+        						 Icon tempImg = lastButtonPressed.getIcon(); //holds the image of the last button pressed
+        						//Image img = ImageIO.read(Game.class.getResource("/images/BlueCircle.png")); //will have to change image src to variable so it can work with any token
         						lastButtonPressed.setIcon(null); //sets the tile with the image null
-        						currentButton.setIcon(new ImageIcon(img)); //move image to tile that was clicked
+        						currentButton.setIcon(tempImg/*new ImageIcon(img)*/); //move image to tile that was clicked
         						lastButtonPressed.revalidate(); //resets and updates button
         						currentButton.revalidate();     //resets and updates button
-        			        	}catch (IOException ex){}
+        			        	//}catch (IOException ex){}
         					currentButton=null; //resets the ActionListener
         					}
         				}	
@@ -78,10 +79,52 @@ public class Game extends JPanel{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         
-        try {//set initial icon location
-        Image img = ImageIO.read(Game.class.getResource("/images/BlueCircle.png"));
-        tile[22].setIcon(new ImageIcon(img));
-        }catch (IOException ex) {}
+        try {//set initial icon locations
+        Image BCircleImg = ImageIO.read(Game.class.getResource("/images/BlueCircle.png")); 
+        tile[0].setIcon(new ImageIcon(BCircleImg));
+        Image BTriangleImg = ImageIO.read(Game.class.getResource("/images/BlueTriangle.png"));
+        tile[1].setIcon(new ImageIcon(BTriangleImg));
+        Image BStarImg = ImageIO.read(Game.class.getResource("/images/BlueStar.png"));
+        tile[2].setIcon(new ImageIcon(BStarImg));
+        Image BPentagonImg = ImageIO.read(Game.class.getResource("/images/BluePentagon.png"));
+        tile[3].setIcon(new ImageIcon(BPentagonImg));
+        Image BSquareImg = ImageIO.read(Game.class.getResource("/images/BlueSquare.png"));
+        tile[4].setIcon(new ImageIcon(BSquareImg));
+        
+        Image GCircleImg = ImageIO.read(Game.class.getResource("/images/GreenCircle.png"));
+        tile[99].setIcon(new ImageIcon(GCircleImg));
+        Image GTriangleImg = ImageIO.read(Game.class.getResource("/images/GreenTriangle.png"));
+        tile[98].setIcon(new ImageIcon(GTriangleImg));
+        Image GStarImg = ImageIO.read(Game.class.getResource("/images/GreenStar.png"));
+        tile[97].setIcon(new ImageIcon(GStarImg));
+        Image GPentagonImg = ImageIO.read(Game.class.getResource("/images/GreenPentagon.png"));
+        tile[96].setIcon(new ImageIcon(GPentagonImg));
+        Image GSquareImg = ImageIO.read(Game.class.getResource("/images/GreenSquare.png"));
+        tile[95].setIcon(new ImageIcon(GSquareImg));
+        
+        Image YCircleImg = ImageIO.read(Game.class.getResource("/images/YellowCircle.png"));
+        tile[90].setIcon(new ImageIcon(YCircleImg));
+        Image YTriangleImg = ImageIO.read(Game.class.getResource("/images/YellowTriangle.png"));
+        tile[91].setIcon(new ImageIcon(YTriangleImg));
+        Image YStarImg = ImageIO.read(Game.class.getResource("/images/YellowStar.png"));
+        tile[92].setIcon(new ImageIcon(YStarImg));
+        Image YPentagonImg = ImageIO.read(Game.class.getResource("/images/YellowPentagon.png"));
+        tile[93].setIcon(new ImageIcon(YPentagonImg));
+        Image YSquareImg = ImageIO.read(Game.class.getResource("/images/YellowSquare.png"));
+        tile[94].setIcon(new ImageIcon(YSquareImg));
+        
+        Image RCircleImg = ImageIO.read(Game.class.getResource("/images/RedCircle.png"));
+        tile[9].setIcon(new ImageIcon(RCircleImg));
+        Image RTriangleImg = ImageIO.read(Game.class.getResource("/images/RedTriangle.png"));
+        tile[8].setIcon(new ImageIcon(RTriangleImg));
+        Image RStarImg = ImageIO.read(Game.class.getResource("/images/RedStar.png"));
+        tile[7].setIcon(new ImageIcon(RStarImg));
+        Image RPentagonImg = ImageIO.read(Game.class.getResource("/images/RedPentagon.png"));
+        tile[6].setIcon(new ImageIcon(RPentagonImg));
+        Image RSquareImg = ImageIO.read(Game.class.getResource("/images/RedSquare.png"));
+        tile[5].setIcon(new ImageIcon(RSquareImg));
+        }catch (IOException ex) {} //This will eventually need to be taken care of by a function so it can load the choices of the user
+        
     }  
   }
 
